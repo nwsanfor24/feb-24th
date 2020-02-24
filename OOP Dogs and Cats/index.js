1364878d9072ed70f272e1689c09a7824c10f713
Nathan Sanford <nwsanfor24@gmail.com>
2020-02-24 18:53:38 -0500

    made new stuff

--- OOP Dogs and Cats/index.js
+++ OOP Dogs and Cats/index.js
@@ -1,22 +1,30 @@
 const dogs = {
-    "raining": true,
-    "noise": "Woof!",
-    "makeNoise": () => {
-        console.log(dogs.noise)
+    raining: true,
+    noise: "Woof!",
+    makeNoise: () => {
+        if (this.raining === true) {
+            console.log(this.noise);
+        }
     }
 }
 
 const cats = {
-    "raining": false,
-    "noise": "Meow!",
-    "makeNoise": () => {
-        console.log(cats.noise)
+    raining: false,
+    noise: "Meow!",
+    makeNoise: () => {
+        if (this.raining === true) {
+            console.log(this.noise);
+        }
     }
 }
 
-console.log(dogs.makeNoise());
+dogs.makeNoise();
+cats.raining = true;
+cats.makeNoise();
 
-console.log(cats.makeNoise());
-
-
+var massHysteria = function(dogs, cats) {
+    if (dogs.raining === true && cats.raining === true) {
+        console.log("DOGS AND CATS, LIVING TOGETHER, MASS HYSTERIA!!!!");
+    }
+}
 
